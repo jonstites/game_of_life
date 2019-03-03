82b8e2cfe29b1f9855518bba9f5d7f8b71dbecb3
Jonathan Stites <mail@jonstites.com>
2019-03-03 09:09:28 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {:?}!", name));
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+
+impl Universe {
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        4
+    }
+}
