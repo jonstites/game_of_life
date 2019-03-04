55df445ea358a0cd202e6fa4a9afb2382b731366
Jonathan Stites <mail@jonstites.com>
2019-03-04 07:28:21 -0600

    Adds some tests for game of life

--- src/lib.rs
+++ src/lib.rs
@@ -46,8 +46,8 @@ pub struct Universe {
 impl Universe {
 
     pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
+        let width = 180;
+        let height = 180;
 
         let cells = (0..width * height)
             .map(|i| {
@@ -63,6 +63,16 @@ impl Universe {
             height,
             cells,
         }
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();        
     }
 
     pub fn width(&self) -> u32 {
@@ -126,6 +136,20 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+}
+
+impl Universe {
+
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
